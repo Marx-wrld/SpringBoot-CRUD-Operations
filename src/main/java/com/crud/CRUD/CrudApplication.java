c25ffb85fa20a9ea180a14d513a25cd664d5b185
Marx-wrld <iongonga@kabarak.ac.ke>
2024-02-06 20:47:23 +0300

    CRUD Operations with springboot

---
+++ src/main/java/com/crud/CRUD/CrudApplication.java
@@ -0,0 +1,14 @@
+package com.crud.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crud/CRUD/user/UserRepository.java
@@ -0,0 +1,5 @@
+package com.crud.CRUD.user;
+
+public class UserRepository {
+    
+}
